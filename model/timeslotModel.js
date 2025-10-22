1a1d6381a676304a57d96d372425867e2750e3d9
ChromaCyan <jofe.bugarin.up@phinmaed.com>
2025-10-22 14:42:32 +0800

    Fixing missing timeslot

---
+++ model/timeslotModel.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const timeSlotSchema = new mongoose.Schema({
+  specialist: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  dayOfWeek: {
+    type: String,
+    enum: [
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+      "Sunday",
+    ],
+    required: true,
+  },
+  startTime: {
+    type: String, 
+    required: true,
+  },
+  endTime: {
+    type: String, 
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("TimeSlot", timeSlotSchema);
